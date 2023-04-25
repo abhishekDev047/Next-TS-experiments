12dce20bb579007f51ffb550c1703a725464372d
Abhishek Kumar <abhishekkumar.in047@gmail.com>
2023-04-25 16:05:44 +0530

    done for today

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,60 @@
 import Head from 'next/head'
 import Form from './Form'
+import Accordion from 'react-bootstrap/Accordion';
+import { Button } from 'react-bootstrap';
+import { useState } from 'react';
+
+
+function Show() {
+  return (
+    <Accordion defaultActiveKey="0" flush>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Accordion Item #1</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Accordion Item #2</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
+}
+
 
 export default function Home() {
+  const [items, setItems] = useState([])
+  const boom= (a:string, b:string)=> {
+    if(a===b)
+    return console.log('yes you are right');
+    return console.log('not good ') ;
+  };
+  
+   const get = async ()=>{
+    let url:string = 'https://jsonplaceholder.typicode.com/todos/1';
+    let response = await fetch(url);
+    let data= await response.json()
+    if(!!data)
+    return setItems(data),console.log(data);
+    return console.log('not found ')
+   }
+   console.log(items);
+   
   return (
     <>
       <Head>
@@ -12,6 +65,10 @@ export default function Home() {
       </Head>
       <div className='container'>
         <Form/>
+        <Show/>
+        <Button variant='success' onClick={()=>boom('meri','meris')}>boom</Button>
+        <Button variant='success' className='mx-2' onClick={get}>GEt</Button>
+        
       </div>
     </>
   )
